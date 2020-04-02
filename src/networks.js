89d2a97058c7ba66c6db095901e1a814a908f3bb
lubhub612 <lundhakgithub@gmail.com>
2020-04-02 07:22:13 +0000

    first commit

--- src/networks.js
+++ src/networks.js
@@ -235,6 +235,27 @@ module.exports = {
     coin: coins.ZEC,
     hashFunctions: hashFunctions
   },
+z vcoin: {
+    messagePrefix: '\x18Vcoin Signed Message:\n',
+    bech32: 'bc',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    pubKeyHash: 0x0fc5,
+    scriptHash: 0x0fc9,
+    wif: 0x80,
+    // This parameter was introduced in version 3 to allow soft forks, for version 1 and 2 transactions we add a
+    // dummy value.
+    consensusBranchId: {
+      1: 0x00,
+      2: 0x00,
+      3: 0x5ba81b19,
+      4: 0x76b809bb
+    },
+    coin: coins.ZEC,
+    hashFunctions: hashFunctions
+  },
   zero: {
     messagePrefix: '\x18Zero Signed Message:\n',
     bech32: 'bc',
